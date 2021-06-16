431e47b230781e8fcf47486fdc461f3331969a85
Jonathan Quintero Giraldo <jonathan.quintero@newshore.es>
2021-06-15 19:00:00 -0500

    my first react app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <h1>Mi primera aplicación</h1>
+          <p>"Esta es mi primera aplicación en React, está padrísimo!"</p>
+        </div>      
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
